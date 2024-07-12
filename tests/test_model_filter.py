4b22e25a5d0116792b140020e91e93107f1ebabd
Diorge Brognara <diorge.bs@gmail.com>
2024-07-12 16:16:04 -0300

    Extra test for model filter

--- tests/test_model_filter.py
+++ tests/test_model_filter.py
@@ -1,6 +1,6 @@
 """Test model filters."""
 
-from dbt_score.model_filter import model_filter
+from dbt_score.model_filter import ModelFilter, model_filter
 from dbt_score.models import Model
 
 
@@ -13,6 +13,21 @@ def test_basic_filter(model1, model2):
         return model.name == "model1"
 
     instance = only_model1()  # since the decorator returns a Type
+    assert instance.description == "Some description."
+    assert instance.evaluate(model1)
+    assert not instance.evaluate(model2)
+
+
+def test_class_filter(model1, model2):
+    """Test basic filter using class."""
+
+    class OnlyModel1(ModelFilter):
+        description = "Some description."
+
+        def evaluate(self, model: Model) -> bool:
+            return model.name == "model1"
+
+    instance = OnlyModel1()
     assert instance.description == "Some description."
     assert instance.evaluate(model1)
     assert not instance.evaluate(model2)
