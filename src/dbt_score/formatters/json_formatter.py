326fb11b36fcb4689afdfa7e7c9d36271cbbffea
Matthieu Caneill <matthieu.caneill@teampicnic.com>
2024-08-07 16:30:51 +0200

    Add JSON formatter for machine-readable output

---
+++ src/dbt_score/formatters/json_formatter.py
@@ -0,0 +1,99 @@
+"""JSON formatter.
+
+Shape of the JSON output:
+
+{
+    "models": {
+        "model_foo": {
+            "score": 0.5,
+            "badge": "🥈",
+            "results": {
+                "rule1": {
+                    "result": "OK",
+                    "severity": null
+                    "message": null
+                },
+                "rule2": {
+                    "result": "WARN",
+                    "severity": "medium",
+                    "message": "Model lacks a description."
+                }
+            ]
+        },
+        "model_bar": {
+            "score": 0.0,
+            "badge": "🥉",
+            "results": {
+                "rule1": {
+                    "result": "ERR",
+                    "message": "Exception message"
+                }
+            }
+        }
+    },
+    "project": {
+        "score": 0.25,
+        "badge": "🥉"
+    }
+}
+"""
+
+
+import json
+from typing import Any
+
+from dbt_score.evaluation import ModelResultsType
+from dbt_score.formatters import Formatter
+from dbt_score.models import Model
+from dbt_score.rule import RuleViolation
+from dbt_score.scoring import Score
+
+
+class JSONFormatter(Formatter):
+    """Formatter for JSON output."""
+
+    def __init__(self, *args: Any, **kwargs: Any):
+        """Instantiate formatter."""
+        super().__init__(*args, **kwargs)
+        self._model_results: dict[str, dict[str, Any]] = {}
+        self._project_results: dict[str, Any]
+
+    def model_evaluated(
+        self, model: Model, results: ModelResultsType, score: Score
+    ) -> None:
+        """Callback when a model has been evaluated."""
+        self._model_results[model.name] = {
+            "score": score.value,
+            "badge": score.badge,
+            "results": {},
+        }
+        for rule, result in results.items():
+            if result is None:
+                self._model_results[model.name]["results"][rule.source()] = {
+                    "result": "OK",
+                    "severity": None,
+                    "message": None,
+                }
+            elif isinstance(result, RuleViolation):
+                self._model_results[model.name]["results"][rule.source()] = {
+                    "result": "WARN",
+                    "severity": rule.severity.name.lower(),
+                    "message": result.message,
+                }
+            else:
+                self._model_results[model.name]["results"][rule.source()] = {
+                    "result": "ERR",
+                    "message": str(result),
+                }
+
+    def project_evaluated(self, score: Score) -> None:
+        """Callback when a project has been evaluated."""
+        self._project_results = {
+            "score": score.value,
+            "badge": score.badge,
+        }
+        document = {
+            "models": self._model_results,
+            "project": self._project_results,
+        }
+        print(json.dumps(document, indent=2, ensure_ascii=False))
---
+++ tests/formatters/test_json_formatter.py
@@ -0,0 +1,60 @@
+"""Unit tests for the JSON formatter."""
+
+from typing import Type
+
+from dbt_score.formatters.json_formatter import JSONFormatter
+from dbt_score.rule import Rule, RuleViolation
+from dbt_score.scoring import Score
+
+
+def test_json_formatter(
+    capsys,
+    default_config,
+    manifest_loader,
+    model1,
+    rule_severity_low,
+    rule_severity_medium,
+    rule_severity_critical,
+):
+    """Ensure the formatter has the correct output after model evaluation."""
+    formatter = JSONFormatter(manifest_loader=manifest_loader, config=default_config)
+    results: dict[Type[Rule], RuleViolation | Exception | None] = {
+        rule_severity_low: None,
+        rule_severity_medium: Exception("Oh noes"),
+        rule_severity_critical: RuleViolation("Error"),
+    }
+    formatter.model_evaluated(model1, results, Score(10.0, "🥇"))
+    formatter.project_evaluated(Score(10.0, "🥇"))
+    stdout = capsys.readouterr().out
+    assert (
+        stdout
+        == """{
+  "models": {
+    "model1": {
+      "score": 10.0,
+      "badge": "🥇",
+      "results": {
+        "tests.conftest.rule_severity_low": {
+          "result": "OK",
+          "severity": null,
+          "message": null
+        },
+        "tests.conftest.rule_severity_medium": {
+          "result": "ERR",
+          "message": "Oh noes"
+        },
+        "tests.conftest.rule_severity_critical": {
+          "result": "WARN",
+          "severity": "critical",
+          "message": "Error"
+        }
+      }
+    }
+  },
+  "project": {
+    "score": 10.0,
+    "badge": "🥇"
+  }
+}
+"""
+    )
