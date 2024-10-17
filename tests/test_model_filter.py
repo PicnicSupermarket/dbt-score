aaeccbdebc8e84500ab3f19b4239bcbed7a7d7f0
Oliver Tosky <olivertosky@gmail.com>
2024-10-16 23:45:29 -0400

    coverage

--- tests/test_model_filter.py
+++ tests/test_model_filter.py
@@ -1,5 +1,5 @@
 """Test model filters."""
-
+import pytest
 from dbt_score.model_filter import ModelFilter, model_filter
 from dbt_score.models import Model, Source
 
@@ -60,3 +60,34 @@ def test_class_filter_with_sources(source1, source2):
     assert instance.description == "Some description."
     assert instance.evaluate(source1)
     assert not instance.evaluate(source2)
+
+
+def test_missing_description_rule_filter():
+    """Test missing description in filter decorator."""
+    with pytest.raises(AttributeError):
+
+        @model_filter()
+        def example_filter(model: Model) -> bool:
+            return True
+
+
+def test_missing_description_rule_class():
+    """Test missing description in filter class."""
+    with pytest.raises(AttributeError):
+
+        class BadFilter(ModelFilter):
+            """Bad example filter."""
+
+            def evaluate(self, model: Model) -> bool:
+                """Evaluate filter."""
+                return True
+
+
+def test_missing_evaluate_rule_class(model1):
+    """Test missing evaluate implementation in filter class."""
+    with pytest.raises(TypeError):
+
+        class BadFilter(ModelFilter):
+            """Bad example filter."""
+
+            description = "Description of the rule."
