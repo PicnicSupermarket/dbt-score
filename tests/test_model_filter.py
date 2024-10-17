fe80bdfcd830ca608bc7c458764cabdc6a748aae
Oliver Tosky <olivertosky@gmail.com>
2024-10-16 23:35:23 -0400

    allow sources to be filtered

--- tests/test_model_filter.py
+++ tests/test_model_filter.py
@@ -1,7 +1,7 @@
 """Test model filters."""
 
 from dbt_score.model_filter import ModelFilter, model_filter
-from dbt_score.models import Model
+from dbt_score.models import Model, Source
 
 
 def test_basic_filter(model1, model2):
@@ -18,6 +18,20 @@ def test_basic_filter(model1, model2):
     assert not instance.evaluate(model2)
 
 
+def test_basic_filter_with_sources(source1, source2):
+    """Test basic filter testing for a specific model."""
+
+    @model_filter
+    def only_source1(source: Source) -> bool:
+        """Some description."""
+        return source.name == "table1"
+
+    instance = only_source1()  # since the decorator returns a Type
+    assert instance.description == "Some description."
+    assert instance.evaluate(source1)
+    assert not instance.evaluate(source2)
+
+
 def test_class_filter(model1, model2):
     """Test basic filter using class."""
 
@@ -31,3 +45,18 @@ def test_class_filter(model1, model2):
     assert instance.description == "Some description."
     assert instance.evaluate(model1)
     assert not instance.evaluate(model2)
+
+
+def test_class_filter_with_sources(source1, source2):
+    """Test basic filter using class."""
+
+    class OnlySource1(ModelFilter):
+        description = "Some description."
+
+        def evaluate(self, source: Source) -> bool:
+            return source.name == "table1"
+
+    instance = OnlySource1()
+    assert instance.description == "Some description."
+    assert instance.evaluate(source1)
+    assert not instance.evaluate(source2)
